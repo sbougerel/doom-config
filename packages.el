26a627a4630d3432365b9a471d4513da56f9d660
Sylvain Bougerel <sylvain.bougerel@gmail.com>
2025-03-01 03:11:56 +0800

    chore(eglot): fixed in emacs 30 normally

--- packages.el
+++ packages.el
@@ -50,8 +50,6 @@
            :repo "sbougerel/logseq-org-roam"
            :files ("*.el")))
 
-(package! treesit-auto)
-
 ;; Doom's packages are pinned to a specific commit and updated from release to
 ;; release. The `unpin!' macro allows you to unpin single packages...
 ;(unpin! pinned-package)
